763d23bf0ad99ea45970237a0b4fb35a60e72b66
Ofek <ofek4430@gmail.com>
2025-03-03 13:33:49 +0200

    feat: add chat module (#4)

---
+++ apps/backend/src/modules/chat/chat.controller.ts
@@ -0,0 +1,26 @@
+import { Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import { getChats, getChatDetails } from "./chat.service";
+import { AuthenticatedRequest } from "@repo/backend/middlewares/auth";
+
+const getChatsController = async (req: AuthenticatedRequest, res: Response) => {
+  const chats = await getChats(req.user.id);
+  res.status(StatusCodes.OK).json({ chats });
+};
+
+const getChatDetailsController = async (
+  req: AuthenticatedRequest,
+  res: Response
+) => {
+  const { chatId } = req.params;
+  const { page, limit } = req.query;
+  const chatDetails = await getChatDetails(
+    req.user.id,
+    parseInt(chatId),
+    parseInt(page as string),
+    parseInt(limit as string)
+  );
+  res.status(StatusCodes.OK).json({ chatDetails });
+};
+
+export { getChatsController, getChatDetailsController };
