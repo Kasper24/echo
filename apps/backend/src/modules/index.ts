458eec9e12d1eb413e8251688668b42813e64e85
Ofek <ofek4430@gmail.com>
2025-03-03 13:28:40 +0200

    feat: add friends module (#3)

--- apps/backend/src/modules/index.ts
+++ apps/backend/src/modules/index.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
+import authHandler from "@repo/backend/middlewares/auth";
 import authRouter from "@repo/backend/modules/auth/auth.route";
+import friendsRouter from "@repo/backend/modules/friends/friends.route";
 
 const rootRouter = Router();
 
 rootRouter.use("/auth", authRouter);
+rootRouter.use("/friends", authHandler, friendsRouter);
 
 export default rootRouter;
