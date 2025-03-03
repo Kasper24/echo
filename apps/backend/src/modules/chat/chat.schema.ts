6b98334fa14f58b0edfab95ff64d0633ee5facfb
Ofek <ofek4430@gmail.com>
2025-03-03 13:33:49 +0200

    feat: add chat module (#4)

---
+++ apps/backend/src/modules/chat/chat.schema.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+const getChatDetailsSchema = z.object({
+  params: z.object({
+    id: z.coerce.number().int(),
+  }),
+  query: z.object({
+    page: z.coerce.number().positive().int().default(1),
+    limit: z.coerce.number().positive().int().default(50),
+  }),
+});
+
+export { getChatDetailsSchema };
