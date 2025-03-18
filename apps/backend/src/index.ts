e97f8641649d59f22b81c8f3abfe3356f03f1dee
Ofek Itscovits <ofek4430@gmail.com>
2025-03-18 11:00:01 +0200

    refactor: replace redios package with ioredis

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,11 +1,9 @@
 import { createServer } from "@repo/backend/server";
-import { redisConnect } from "@repo/backend/redis";
 import envValidate from "@repo/backend/config";
 import { dbTestConnection } from "@repo/database";
 
 await envValidate();
 await dbTestConnection();
-await redisConnect();
 
 const server = createServer();
 server.listen(process.env.PORT, () => {
