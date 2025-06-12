1a7e61383b1126d818b1280f8add59ae27bb3d2c
Ofek Itscovits <ofek4430@gmail.com>
2025-06-12 11:30:58 +0300

    feat: add new user routes
    * Add new privacy settings get and patch handlers
    * Add new user delete route handler

--- apps/api/src/modules/user/user.route.ts
+++ apps/api/src/modules/user/user.route.ts
@@ -6,8 +6,13 @@ import {
   createTypiRouter,
 } from "@repo/typiserver";
 import { db } from "@repo/database";
-import { users } from "@repo/database/schema";
+import {
+  refreshTokens,
+  userPrivacySettings,
+  users,
+} from "@repo/database/schema";
 import authMiddleware from "@repo/api/middlewares/auth";
+import { createUpdateSchema } from "drizzle-zod";
 
 const userRouter = createTypiRouter({
   "/": createTypiRoute({
@@ -47,6 +52,71 @@ const userRouter = createTypiRouter({
 
         return ctx.success({
           user: user[0],
+        });
+      },
+    }),
+    delete: createTypiRouteHandler({
+      middlewares: [authMiddleware],
+      handler: async (ctx) => {
+        const deleted = await db
+          .delete(users)
+          .where(eq(users.id, ctx.data.userId))
+          .returning();
+
+        if (!deleted.length) return ctx.error("NOT_FOUND", "User not found.");
+
+        await db
+          .delete(refreshTokens)
+          .where(eq(refreshTokens.userId, ctx.data.userId));
+
+        ctx.response.clearCookie(process.env.JWT_ACCESS_TOKEN_COOKIE_KEY);
+        ctx.response.clearCookie(process.env.JWT_REFRESH_TOKEN_COOKIE_KEY);
+
+        return ctx.success({
+          message: "User deleted successfully.",
+        });
+      },
+    }),
+  }),
+  "/privacy": createTypiRoute({
+    get: createTypiRouteHandler({
+      middlewares: [authMiddleware],
+      handler: async (ctx) => {
+        const privacySettings = await db.query.userPrivacySettings.findFirst({
+          where: and(eq(users.id, ctx.data.userId)),
+        });
+        if (!privacySettings) return ctx.error("NOT_FOUND", "User not found.");
+
+        return ctx.success({
+          privacySettings,
+        });
+      },
+    }),
+    patch: createTypiRouteHandler({
+      input: {
+        body: z.object({
+          privacySettings: createUpdateSchema(userPrivacySettings, {
+            createdAt: z.undefined(),
+            updatedAt: z.undefined(),
+          }),
+        }),
+      },
+      middlewares: [authMiddleware],
+      handler: async (ctx) => {
+        const updatedPrivacySettings = await db
+          .update(userPrivacySettings)
+          .set({
+            updatedAt: new Date(),
+            ...ctx.input.body.privacySettings,
+          })
+          .where(eq(userPrivacySettings.userId, ctx.data.userId))
+          .returning();
+
+        if (!updatedPrivacySettings.length)
+          return ctx.error("NOT_FOUND", "User not found.");
+
+        return ctx.success({
+          privacySettings: updatedPrivacySettings[0],
         });
       },
     }),
